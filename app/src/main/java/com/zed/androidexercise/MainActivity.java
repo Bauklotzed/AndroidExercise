a58dfd8aa5ac07ac00bdfd21c7f767aa84156a46
zed <haia5173@163.com>
2019-08-22 10:27:01 +0800

    新增xml解析

--- app/src/main/java/com/zed/androidexercise/MainActivity.java
+++ app/src/main/java/com/zed/androidexercise/MainActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
 
 import okhttp3.Call;
@@ -23,8 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView responseText;
 
-    private String address = "http://10.0.2.2/get_url.json";
-    private String id = "1000";
+    private String address = "http://10.0.2.2/get_url.xml";
+    private String id = "1002";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,12 +64,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // 判断是json还是xml格式，并进行相应的处理
-    // TODO:xml解析
+    // TODO:文件不为xml或json时的处理
     private String parse(String id, String responseData) {
         if (address.endsWith(".json")) {
             return parseJSONWithGSON(id, responseData);
+        } else if (address.endsWith("xml")) {
+            return parseXMLWithPull(id, responseData);
         }
-        return "error";
+        return "error:wrong format";
     }
 
     // 解析json，根据id返回相应网址
@@ -79,7 +84,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return website.getUrl();
             }
         }
-        return "not find";
+        return "error:not found";
+    }
+
+    // 解析xml，根据id返回相应网址
+    // TODO:未找到对应网址时处理与异常处理
+    private String parseXMLWithPull(String id, String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String websiteId = "";
+            String url = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if ("id".equals(nodeName)) {
+                            websiteId = xmlPullParser.nextText();
+                        } else if ("url".equals(nodeName)) {
+                            url = xmlPullParser.nextText();
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if ("website".equals(nodeName) && websiteId.equals(id)) {
+                            return url;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            return "error:" + e.getMessage();
+        }
+        // 未找到对应网址
+        return "error:not found";
     }
 
     // 打开浏览器访问指定网址
